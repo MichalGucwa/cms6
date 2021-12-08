4c4435d2ce2cef0e5919e207c57c4cc640a791d7
michal_g <michal_g@iwonka.med.virginia.edu>
2021-12-08 13:38:43 -0500

    start

---
+++ src/Controller/MetalsController.php
@@ -0,0 +1,17 @@
+<?php
+// src/Controller/ArticlesController.php
+
+namespace App\Controller;
+
+class MetalsController extends AppController
+{
+    public function index()
+    {
+
+    }
+
+    public function metals()
+    {
+
+    }
+}
---
+++ templates/Metals/metals.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    metals
+    <?= $this->Html->link('index', ['action' => 'index']) ?>
+  </body>
+</html>
